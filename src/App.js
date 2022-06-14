6d553bd155a3f6bbc67b129dc58c568bcdc45d64
Shannon Yazdani <shannonyazdani@gmail.com>
2022-06-13 22:31:07 -0400

    added bare bones structure for warehouse list item for warehouses component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.scss";
+import WHList from "./components/WHList/WHList";
 
 function App() {
   return (
     <div className="App">
       <h1>This is a font test</h1>
+      <WHList />
     </div>
   );
 }
