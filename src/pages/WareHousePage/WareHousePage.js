b2cdd9f242dd64c67918cd56f945973512cba30a
Michael Nguyen <n93michael@gmail.com>
2022-06-14 16:08:06 -0400

    added in warehouseadd component

--- src/pages/WareHousePage/WareHousePage.js
+++ src/pages/WareHousePage/WareHousePage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import WarehouseList from "../../components/WarehouseComponents/WarehouseList/WarehouseList";
+import WarehouseAdd from "../../components/WarehouseComponents/WarehouseAdd/WarehouseAdd";
 
 function WareHousePage() {
   return (
     <div>
       <WarehouseList />
+      <WarehouseAdd />
     </div>
   );
 }
