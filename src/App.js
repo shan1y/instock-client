b0bfa63a31d25c81e354af06553f4641743da305
yakiv-k <yakiv.y.k@gmail.com>
2022-06-13 23:04:56 -0400

    FAM-3 FRONT-END installed react + routing package. Setup basic routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+// import WareHousePage from "";
+// import InventoryPage from "";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class React extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route to="/" exact component={WareHousePage}></Route>
+          <Route to="/inventory" component={InventoryPage}></Route>
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
