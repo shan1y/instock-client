e2858c6de4fceca77f19bdd7929045f573cd3412
Shannon Yazdani <shannonyazdani@gmail.com>
2022-06-16 21:04:35 -0400

    added some warehouse page header

--- src/pages/WareHousePage/WareHousePage.js
+++ src/pages/WareHousePage/WareHousePage.js
@@ -1,12 +1,13 @@
 import React from "react";
 import WarehouseList from "../../components/WarehouseComponents/WarehouseList/WarehouseList";
 // import WarehouseAdd from "../../components/WarehouseComponents/WarehouseAdd/WarehouseAdd";
+import "./WarehousePage.scss";
 
 class WareHousePage extends React.Component {
   render() {
     return (
       <div>
-        <div className="InventoryList__body">
+        <div className="WarehouseList__body">
           <WarehouseList />
         </div>
       </div>
