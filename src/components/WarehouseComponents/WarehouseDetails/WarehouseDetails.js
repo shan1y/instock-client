736f8e5faa0ae892ff4b3d653b43f9acce927bcd
Michael Nguyen <n93michael@gmail.com>
2022-06-17 16:31:49 -0400

    delete modal moved to warehousedetails

--- src/components/WarehouseComponents/WarehouseDetails/WarehouseDetails.js
+++ src/components/WarehouseComponents/WarehouseDetails/WarehouseDetails.js
@@ -4,6 +4,7 @@ import axios from "axios";
 import { Link } from "react-router-dom";
 import chevron from "../../../assets/Icons/chevron_right-24px.svg";
 import backArrow from "../../../assets/Icons/arrow_back-24px.svg";
+import DeleteModal from "../../DeleteModal/DeleteModal";
 
 class WarehouseDetails extends Component {
   state = {
@@ -21,6 +22,8 @@ class WarehouseDetails extends Component {
       },
     },
     warehouseInventory: [],
+    isOpen: false,
+    activeInventoryId: null,
   };
 
   componentDidMount() {
@@ -45,16 +48,48 @@ class WarehouseDetails extends Component {
 
   stockCheck = (stock) => (stock === 0 ? "OUT OF STOCK" : "IN STOCK");
 
+  openModal = (id) => {
+    this.setState({ isOpen: true, activeInventoryId: id });
+  };
+
+  closeModal = () => this.setState({ isOpen: false });
+
+  deleteItem = (id) => {
+    axios
+      .delete(`http://localhost:8080/inventory/${id}`)
+
+      .then((response) => {
+        this.setState({ warehouseInventory: response.data, isOpen: false });
+      });
+  };
+
   render() {
     const { city, address, country, contact } = this.state.warehouseDetails;
     const { name, position, phone, email } = contact;
 
+    const activeInventoryId = this.state.activeInventoryId;
+    // let modalData = this.state.warehouseInventory.find((inventory) => {
+    //   return activeInventoryId === inventory.id;
+    // });
+    if (this.state.isOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "unset";
+    }
     console.log(this.state.warehouseInventory);
 
     return (
       <>
-        {this.state?.warehouseDetails ? (
+        {this.state?.warehouseDetails && this.state?.warehouseInventory ? (
           <>
+            <DeleteModal
+              deleteItem={this.deleteItem}
+              closeModal={this.closeModal}
+              isOpen={this.state.isOpen}
+              // warehouseName={modalData.itemName}
+              id={this.state.activeInventoryId}
+            />
+
             <div className="new-warehouse__header">
               <Link className="new-warehouse__link" to="/warehouse">
                 <img
