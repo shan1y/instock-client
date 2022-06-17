be034d1b1c496cdad5a133f67f33361664b985f7
Michael Nguyen <n93michael@gmail.com>
2022-06-17 15:52:37 -0400

    fixed routing in app

--- src/App.js
+++ src/App.js
@@ -9,7 +9,9 @@ import WareHousePage from "./pages/WareHousePage/WareHousePage";
 import WarehouseDetails from "./components/WarehouseComponents/WarehouseDetails/WarehouseDetails";
 import InventoryPage from "./pages/InventoryPage/InventoryPage";
 import WarehouseAdd from "./components/WarehouseComponents/WarehouseAdd/WarehouseAdd";
+import WarehouseEdit from "./components/WarehouseComponents/WarehouseEdit/WarehouseEdit";
 import InventoryItem from "./components/InventoryComponents/InventoryItem/InventoryItem";
+import InventoryEdit from "./components/InventoryComponents/InventoryEdit/InventoryEdit";
 
 class App extends React.Component {
   render() {
@@ -19,12 +21,14 @@ class App extends React.Component {
         <Switch>
           <Route path="/" exact component={WareHousePage} />
           <Route path="/warehouse" exact component={WareHousePage} />
-          <Route path="/warehouse/add" exact component={WarehouseAdd} />
+          <Route path="/warehouse/add" component={WarehouseAdd} />
+          <Route path="/warehouse/edit" component={WarehouseEdit} />
           <Route path="/warehouse/:id/inventory" component={WarehouseDetails} />
 
           <Route path="/inventory" exact component={InventoryPage} />
           <Route path="/inventory/add" exact component={InventoryAdd} />
           <Route path="/inventory/:id" exact component={InventoryItem} />
+          <Route path="/inventory/edit/:id" exact component={InventoryEdit} />
         </Switch>
         <FooterSection />
       </BrowserRouter>
