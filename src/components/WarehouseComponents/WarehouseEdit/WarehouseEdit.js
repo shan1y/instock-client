23892def8583a30a261c5ed3c944e4e60241a5c3
yakiv-k <yakiv.y.k@gmail.com>
2022-06-17 15:54:53 -0400

    unknown changes

--- src/components/WarehouseComponents/WarehouseEdit/WarehouseEdit.js
+++ src/components/WarehouseComponents/WarehouseEdit/WarehouseEdit.js
@@ -7,166 +7,166 @@ import errorIcon from "../../../assets/Icons/error-24px.svg";
 import axios from "axios";
 
 class WarehouseEdit extends Component {
-  state = {
-    warehouseNameCheck: false,
-    addressCheck: false,
-    cityCheck: false,
-    countryCheck: false,
-    contactNameCheck: false,
-    positionCheck: false,
-    numberCheck: false,
-    emailCheck: false,
-  };
+//   state = {
+//     warehouseNameCheck: false,
+//     addressCheck: false,
+//     cityCheck: false,
+//     countryCheck: false,
+//     contactNameCheck: false,
+//     positionCheck: false,
+//     numberCheck: false,
+//     emailCheck: false,
+//   };
 
-  handleWarehouseName = (event) => {
-    const inputValue = event.target.value;
+//   handleWarehouseName = (event) => {
+//     const inputValue = event.target.value;
 
-    if (inputValue.trim() === "") {
-      this.setState({ warehouseNameCheck: true });
-    } else {
-      this.setState({ warehouseNameCheck: false });
-    }
-  };
+//     if (inputValue.trim() === "") {
+//       this.setState({ warehouseNameCheck: true });
+//     } else {
+//       this.setState({ warehouseNameCheck: false });
+//     }
+//   };
 
-  handleAddress = (event) => {
-    const inputValue = event.target.value;
-    if (inputValue.trim() === "") {
-      this.setState({ addressCheck: true });
-    } else {
-      this.setState({ addressCheck: false });
-    }
-  };
+//   handleAddress = (event) => {
+//     const inputValue = event.target.value;
+//     if (inputValue.trim() === "") {
+//       this.setState({ addressCheck: true });
+//     } else {
+//       this.setState({ addressCheck: false });
+//     }
+//   };
 
-  handleCity = (event) => {
-    const inputValue = event.target.value;
+//   handleCity = (event) => {
+//     const inputValue = event.target.value;
 
-    if (inputValue.trim() === "") {
-      this.setState({ cityCheck: true });
-    } else {
-      this.setState({ cityCheck: false });
-    }
-  };
+//     if (inputValue.trim() === "") {
+//       this.setState({ cityCheck: true });
+//     } else {
+//       this.setState({ cityCheck: false });
+//     }
+//   };
 
-  handleCountry = (event) => {
-    const inputValue = event.target.value;
+//   handleCountry = (event) => {
+//     const inputValue = event.target.value;
 
-    if (inputValue.trim() === "") {
-      this.setState({ countryCheck: true });
-    } else {
-      this.setState({ countryCheck: false });
-    }
-  };
+//     if (inputValue.trim() === "") {
+//       this.setState({ countryCheck: true });
+//     } else {
+//       this.setState({ countryCheck: false });
+//     }
+//   };
 
-  handleContactName = (event) => {
-    const inputValue = event.target.value;
+//   handleContactName = (event) => {
+//     const inputValue = event.target.value;
 
-    if (inputValue.trim() === "") {
-      this.setState({ contactNameCheck: true });
-    } else {
-      this.setState({ contactNameCheck: false });
-    }
-  };
+//     if (inputValue.trim() === "") {
+//       this.setState({ contactNameCheck: true });
+//     } else {
+//       this.setState({ contactNameCheck: false });
+//     }
+//   };
 
-  handlePosition = (event) => {
-    const inputValue = event.target.value;
+//   handlePosition = (event) => {
+//     const inputValue = event.target.value;
 
-    if (inputValue.trim() === "") {
-      this.setState({ positionCheck: true });
-    } else {
-      this.setState({ positionCheck: false });
-    }
-  };
+//     if (inputValue.trim() === "") {
+//       this.setState({ positionCheck: true });
+//     } else {
+//       this.setState({ positionCheck: false });
+//     }
+//   };
 
-  handlePhoneNumber = (event) => {
-    const inputValue = event.target.value;
-    const numberRegex =
-      /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
-    if (inputValue.trim() === "") {
-      this.setState({ numberCheck: true });
-    } else if (!numberRegex.test(inputValue)) {
-      this.setState({ numberCheck: true });
-    } else {
-      this.setState({ numberCheck: false });
-    }
-  };
+//   handlePhoneNumber = (event) => {
+//     const inputValue = event.target.value;
+//     const numberRegex =
+//       /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+//     if (inputValue.trim() === "") {
+//       this.setState({ numberCheck: true });
+//     } else if (!numberRegex.test(inputValue)) {
+//       this.setState({ numberCheck: true });
+//     } else {
+//       this.setState({ numberCheck: false });
+//     }
+//   };
 
-  handleEmail = (event) => {
-    const inputValue = event.target.value;
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (inputValue.trim() === "") {
-      this.setState({ emailCheck: true });
-    } else if (!emailRegex.test(inputValue)) {
-      this.setState({ emailCheck: true });
-    } else {
-      this.setState({ emailCheck: false });
-    }
-  };
+//   handleEmail = (event) => {
+//     const inputValue = event.target.value;
+//     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+//     if (inputValue.trim() === "") {
+//       this.setState({ emailCheck: true });
+//     } else if (!emailRegex.test(inputValue)) {
+//       this.setState({ emailCheck: true });
+//     } else {
+//       this.setState({ emailCheck: false });
+//     }
+//   };
 
-  handleSubmit = (event) => {
-    event.preventDefault();
+//   handleSubmit = (event) => {
+//     event.preventDefault();
 
-    if (event.target.warehouseName.value.trim() === "") {
-      this.setState({ warehouseNameCheck: true });
-    }
-    if (event.target.address.value.trim() === "") {
-      this.setState({ addressCheck: true });
-    }
+//     if (event.target.warehouseName.value.trim() === "") {
+//       this.setState({ warehouseNameCheck: true });
+//     }
+//     if (event.target.address.value.trim() === "") {
+//       this.setState({ addressCheck: true });
+//     }
 
-    if (event.target.city.value.trim() === "") {
-      this.setState({ cityCheck: true });
-    }
+//     if (event.target.city.value.trim() === "") {
+//       this.setState({ cityCheck: true });
+//     }
 
-    if (event.target.country.value.trim() === "") {
-      this.setState({ countryCheck: true });
-    }
+//     if (event.target.country.value.trim() === "") {
+//       this.setState({ countryCheck: true });
+//     }
 
-    if (event.target.contactName.value.trim() === "") {
-      this.setState({ contactNameCheck: true });
-    }
+//     if (event.target.contactName.value.trim() === "") {
+//       this.setState({ contactNameCheck: true });
+//     }
 
-    if (event.target.position.value.trim() === "") {
-      this.setState({ positionCheck: true });
-    }
+//     if (event.target.position.value.trim() === "") {
+//       this.setState({ positionCheck: true });
+//     }
 
-    if (event.target.phone.value.trim() === "") {
-      this.setState({ numberCheck: true });
-    }
+//     if (event.target.phone.value.trim() === "") {
+//       this.setState({ numberCheck: true });
+//     }
 
-    if (event.target.email.value.trim() === "") {
-      this.setState({ emailCheck: true });
-    }
+//     if (event.target.email.value.trim() === "") {
+//       this.setState({ emailCheck: true });
+//     }
 
-    if (
-      !this.state.warehouseNameCheck &&
-      !this.state.addressCheck &&
-      !this.state.cityCheck &&
-      !this.state.numberCheck &&
-      !this.state.positionCheck &&
-      !this.state.emailCheck &&
-      !this.state.countryCheck &&
-      event.target.warehouseName.value &&
-      event.target.address.value &&
-      event.target.city.value &&
-      event.target.country.value &&
-      event.target.position.value &&
-      event.target.phone.value &&
-      event.target.email.value
-    ) {
-      axios
-        .post("http://localhost:8080/warehouse", {
-          warehouseName: event.target.warehouseName.value,
-          address: event.target.address.value,
-          city: event.target.city.value,
-          country: event.target.country.value,
-          contactName: event.target.contactName.value,
-          position: event.target.position.value,
-          phone: event.target.phone.value,
-          email: event.target.email.value,
-        })
-        .catch((error) => console.log(error));
-      this.props.history.push("/warehouse");
-    }
-  };
+//     if (
+//       !this.state.warehouseNameCheck &&
+//       !this.state.addressCheck &&
+//       !this.state.cityCheck &&
+//       !this.state.numberCheck &&
+//       !this.state.positionCheck &&
+//       !this.state.emailCheck &&
+//       !this.state.countryCheck &&
+//       event.target.warehouseName.value &&
+//       event.target.address.value &&
+//       event.target.city.value &&
+//       event.target.country.value &&
+//       event.target.position.value &&
+//       event.target.phone.value &&
+//       event.target.email.value
+//     ) {
+//       axios
+//         .post("http://localhost:8080/warehouse", {
+//           warehouseName: event.target.warehouseName.value,
+//           address: event.target.address.value,
+//           city: event.target.city.value,
+//           country: event.target.country.value,
+//           contactName: event.target.contactName.value,
+//           position: event.target.position.value,
+//           phone: event.target.phone.value,
+//           email: event.target.email.value,
+//         })
+//         .catch((error) => console.log(error));
+//       this.props.history.push("/warehouse");
+//     }
+//   };
 
 
   render() {
@@ -184,7 +184,9 @@ class WarehouseEdit extends Component {
             <h2 className="new-warehouse__title">Edit Warehouse</h2>
           </div>
 
-          <form className="new-warehouse-form" onSubmit={this.handleSubmit}>
+          <form className="new-warehouse-form" 
+        //   onSubmit={this.handleSubmit}
+          >
             <div className="new-warehouse-form__wrapper">
               <div className="new-warehouse-form__left">
                 <div className="new-warehouse-form__container">
@@ -198,10 +200,10 @@ class WarehouseEdit extends Component {
                       name="warehouseName"
                       placeholder="Warehouse Name"
                       className="new-warehouse-form__input"
-                      onChange={this.handleWarehouseName}
+                    //   onChange={this.handleWarehouseName}
                     />
                   </label>
-                  {this.state.warehouseNameCheck && (
+                  {/* {this.state.warehouseNameCheck && (
                     <div className="error">
                       <img
                         src={errorIcon}
@@ -210,7 +212,7 @@ class WarehouseEdit extends Component {
                       />
                       <p className="error__text">This field is required</p>
                     </div>
-                  )}
+                  )} */}
 
                   <label className="new-warehouse-form__label">
                     Street Address
@@ -219,10 +221,10 @@ class WarehouseEdit extends Component {
                       name="address"
                       placeholder="Street Address"
                       className="new-warehouse-form__input"
-                      onChange={this.handleAddress}
+                    //   onChange={this.handleAddress}
                     />
                   </label>
-                  {this.state.addressCheck && (
+                  {/* {this.state.addressCheck && (
                     <div className="error">
                       <img
                         src={errorIcon}
@@ -231,7 +233,7 @@ class WarehouseEdit extends Component {
                       />
                       <p className="error__text">This field is required</p>
                     </div>
-                  )}
+                  )} */}
 
                   <label className="new-warehouse-form__label">
                     City
@@ -240,10 +242,10 @@ class WarehouseEdit extends Component {
                       name="city"
                       placeholder="City"
                       className="new-warehouse-form__input"
-                      onChange={this.handleCity}
+                    //   onChange={this.handleCity}
                     />
                   </label>
-                  {this.state.cityCheck && (
+                  {/* {this.state.cityCheck && (
                     <div className="error">
                       <img
                         src={errorIcon}
@@ -252,7 +254,7 @@ class WarehouseEdit extends Component {
                       />
                       <p className="error__text">This field is required</p>
                     </div>
-                  )}
+                  )} */}
 
                   <label className="new-warehouse-form__label">
                     Country
@@ -261,10 +263,10 @@ class WarehouseEdit extends Component {
                       name="country"
                       placeholder="Country"
                       className="new-warehouse-form__input"
-                      onChange={this.handleCountry}
+                    //   onChange={this.handleCountry}
                     />
                   </label>
-                  {this.state.countryCheck && (
+                  {/* {this.state.countryCheck && (
                     <div className="error">
                       <img
                         src={errorIcon}
@@ -273,7 +275,7 @@ class WarehouseEdit extends Component {
                       />
                       <p className="error__text">This field is required</p>
                     </div>
-                  )}
+                  )} */}
                 </div>
               </div>
 
@@ -287,10 +289,10 @@ class WarehouseEdit extends Component {
                       name="contactName"
                       placeholder="Contact Name"
                       className="new-warehouse-form__input"
-                      onChange={this.handleContactName}
+                    //   onChange={this.handleContactName}
                     />
                   </label>
-                  {this.state.contactNameCheck && (
+                  {/* {this.state.contactNameCheck && (
                     <div className="error">
                       <img
                         src={errorIcon}
@@ -299,7 +301,7 @@ class WarehouseEdit extends Component {
                       />
                       <p className="error__text">This field is required</p>
                     </div>
-                  )}
+                  )} */}
 
                   <label className="new-warehouse-form__label">
                     Position
@@ -308,10 +310,10 @@ class WarehouseEdit extends Component {
                       name="position"
                       placeholder="Position"
                       className="new-warehouse-form__input"
-                      onChange={this.handlePosition}
+                    //   onChange={this.handlePosition}
                     />
                   </label>
-                  {this.state.positionCheck && (
+                  {/* {this.state.positionCheck && (
                     <div className="error">
                       <img
                         src={errorIcon}
@@ -320,7 +322,7 @@ class WarehouseEdit extends Component {
                       />
                       <p className="error__text">This field is required</p>
                     </div>
-                  )}
+                  )} */}
 
                   <label className="new-warehouse-form__label">
                     Phone Number
@@ -328,10 +330,10 @@ class WarehouseEdit extends Component {
                       name="phone"
                       placeholder="Phone Number"
                       className="new-warehouse-form__input"
-                      onChange={this.handlePhoneNumber}
+                    //   onChange={this.handlePhoneNumber}
                     />
                   </label>
-                  {this.state.numberCheck && (
+                  {/* {this.state.numberCheck && (
                     <div className="error">
                       <img
                         src={errorIcon}
@@ -340,7 +342,7 @@ class WarehouseEdit extends Component {
                       />
                       <p className="error__text">This field is required</p>
                     </div>
-                  )}
+                  )} */}
 
                   <label className="new-warehouse-form__label">
                     Email
@@ -349,10 +351,10 @@ class WarehouseEdit extends Component {
                       name="email"
                       placeholder="Email"
                       className="new-warehouse-form__input"
-                      onChange={this.handleEmail}
+                    //   onChange={this.handleEmail}
                     />
                   </label>
-                  {this.state.emailCheck && (
+                  {/* {this.state.emailCheck && (
                     <div className="error">
                       <img
                         src={errorIcon}
@@ -361,7 +363,7 @@ class WarehouseEdit extends Component {
                       />
                       <p className="error__text">This field is required</p>
                     </div>
-                  )}
+                  )} */}
                 </div>
               </div>
             </div>
