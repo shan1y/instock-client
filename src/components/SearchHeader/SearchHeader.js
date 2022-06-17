3a242acc267e19193989794d0b5803bdd1afa636
yakiv-k <yakiv.y.k@gmail.com>
2022-06-17 14:19:56 -0400

    resolved merge conflict

--- src/components/SearchHeader/SearchHeader.js
+++ src/components/SearchHeader/SearchHeader.js
@@ -1,31 +1,22 @@
 import "./SearchHeader.scss";
 
 function SearchHeader({ title }) {
-<<<<<<< HEAD
   return (
-=======
-
-  
-
-
-  return ( 
     <>
->>>>>>> develop
-  <div className="search">
-    <h1 className="search__title">{(title)}</h1>
-    <form className="search__form form">
-        <label className="form__label" htmlFor="search"></label>
-        <input className="form__input" placeholder="Search..." name="search"></input>
-        <button className="form__button">+ Add New Item</button>
-    </form>
-  </div>
-<<<<<<< HEAD
-  )
-=======
-  </>
-);
-
->>>>>>> develop
+      <div className="search">
+        <h1 className="search__title">{title}</h1>
+        <form className="search__form form">
+          <label className="form__label" htmlFor="search"></label>
+          <input
+            className="form__input"
+            placeholder="Search..."
+            name="search"
+          ></input>
+          <button className="form__button">+ Add New Item</button>
+        </form>
+      </div>
+    </>
+  );
 }
 
 export default SearchHeader;
