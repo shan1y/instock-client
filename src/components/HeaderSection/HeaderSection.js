c46383d38081cbe5a74a2cfb42dcb2d156eaa3ef
yakiv-k <yakiv.y.k@gmail.com>
2022-06-19 16:10:57 -0400

    updates

--- src/components/HeaderSection/HeaderSection.js
+++ src/components/HeaderSection/HeaderSection.js
@@ -1,59 +1,25 @@
 import "./HeaderSection.scss";
 import InStockLogo from "../../assets/Logo/InStock-Logo.svg";
-import { Link, useParams } from "react-router-dom";
-import React from "react";
+import { Link } from "react-router-dom";
 
-class HeaderSection extends React.Component {
-  inventorySelected = (endpoint) => {
-    if (endpoint === "/inventory") {
-      return "nav__links--active";
-    } else {
-      return "nav__links--tablet";
-    }
-  };
-  warehouseSelected = (endpoint) => {
-    if (endpoint === "/warehouse") {
-      return "nav__links--active";
-    } else if (endpoint === "/") {
-      return "nav__links--active";
-    } else {
-      return "nav__links--tablet";
-    }
-  };
-
-  render() {
-    return (
-      <header>
-        <nav className="nav">
-          <Link to="/" className="nav__logo-div">
-            <img
-              className="nav__logo"
-              src={InStockLogo}
-              alt="InStock Logo"
-            ></img>
+function HeaderSection() {
+  return (
+    <header>
+      <nav className="nav">
+        <Link to="/" className="nav__logo-div">
+          <img className="nav__logo" src={InStockLogo} alt="InStock Logo"></img>
+        </Link>
+        <div className="nav__directory">
+          <Link to="/" className="nav__links nav__links--active">
+            Warehouses
           </Link>
-          <div className="nav__directory">
-            <Link
-              to="/"
-              className={`nav__links ${this.warehouseSelected(
-                window.location.pathname
-              )}`}
-            >
-              Warehouses
-            </Link>
-            <Link
-              to="/inventory"
-              className={`nav__links ${this.inventorySelected(
-                window.location.pathname
-              )}`}
-            >
-              Inventory
-            </Link>
-          </div>
-        </nav>
-      </header>
-    );
-  }
+          <Link to="/inventory" className="nav__links nav__links--tablet">
+            Inventory
+          </Link>
+        </div>
+      </nav>
+    </header>
+  );
 }
 
 export default HeaderSection;
