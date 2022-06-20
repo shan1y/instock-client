a8c2e32ca4714615b805efd7787840ae861e6a4a
yakiv-k <yakiv.y.k@gmail.com>
2022-06-19 23:33:28 -0400

    done

--- src/components/HeaderSection/HeaderSection.js
+++ src/components/HeaderSection/HeaderSection.js
@@ -4,45 +4,6 @@ import { NavLink } from "react-router-dom";
 import React from "react";
 
 class HeaderSection extends React.Component {
-<<<<<<< HEAD
-=======
-  state = {
-    inventoryClass: "",
-    warehouseClass: "button__inventory--active",
-  };
-
-  inventorySelected = (endpoint) => {
-    if (endpoint === "/inventory") {
-      return "nav__links--active";
-    } else {
-      return "nav__links--tablet";
-    }
-  };
-  warehouseSelected = (endpoint) => {
-    if (endpoint === "/warehouse") {
-      return "nav__links--active";
-    } else if (endpoint === "/") {
-      return "nav__links--active";
-    } else {
-      return "nav__links--tablet";
-    }
-  };
-
-  activeInventoryPageHandler = () => {
-    this.setState({
-      inventoryClass: "button__inventory--active",
-      warehouseClass: "",
-    });
-  };
-
-  activeWarehousePageHandler = () => {
-    this.setState({
-      inventoryClass: "",
-      warehouseClass: "button__warehouse--active",
-    });
-  };
-
->>>>>>> develop
   render() {
     return (
       <header>
@@ -55,7 +16,6 @@ class HeaderSection extends React.Component {
             ></img>
           </NavLink>
           <div className="nav__directory">
-<<<<<<< HEAD
             <NavLink
               to="/warehouse"
               className="nav__links"
@@ -70,25 +30,6 @@ class HeaderSection extends React.Component {
             >
               Inventory
             </NavLink>
-=======
-            <Link to="/" className="nav__links">
-              <button
-                onClick={this.activeWarehousePageHandler}
-                className={`button__warehouse ${this.state.warehouseClass} `}
-              >
-                {" "}
-                Warehouses
-              </button>
-            </Link>
-            <Link to="/inventory" className="nav__links ">
-              <button
-                onClick={this.activeInventoryPageHandler}
-                className={`button__inventory ${this.state.inventoryClass} `}
-              >
-                Inventory
-              </button>
-            </Link>
->>>>>>> develop
           </div>
         </nav>
       </header>
